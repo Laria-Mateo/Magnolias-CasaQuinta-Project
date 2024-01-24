5aecfc4821c160ceb37d7f15653187e796f051da
Mateo Francisco Laria <mateolaria2021@gmail.com>
2024-01-23 22:30:56 -0300

    cambio en logo

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link as ScrollLink, scroller } from 'react-scroll';
-import { Box, VStack, Flex, Spacer, Link, Collapse, Button } from '@chakra-ui/react';
+import { Box, VStack, Flex, Collapse, Button } from '@chakra-ui/react';
 
 const Menu = () => {
   const scrollToSection = (sectionId) => {
@@ -8,6 +8,7 @@ const Menu = () => {
       duration: 500,
       delay: 0,
       smooth: 'easeInOutQuart',
+      offset: window.innerWidth < 768 ? -100 : -50, // Ajusta estos valores según sea necesario
     });
   };
 
@@ -22,11 +23,28 @@ const Menu = () => {
     setIsOpen(false); // Cerrar el menú después de hacer clic en un enlace
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (isOpen) {
+        // Recalcular el desplazamiento si el menú está abierto y el tamaño de la ventana cambia
+        scrollToSection('seccion1'); // Ajusta el valor de la sección según sea necesario
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [isOpen]);
+
   return (
     <>
       <VStack align="center">
         <Box display={{ base: 'block', md: 'none' }}>
-          <Button onClick={toggleMenu} bg={"#D6C4C0"}>Menú</Button>
+          <Button onClick={toggleMenu} bg="#D6C4C0" mt={'15px'}>
+            Menú
+          </Button>
         </Box>
         <Box display={{ base: 'none', md: 'block' }}>
           <Flex>
@@ -49,10 +67,10 @@ const Menu = () => {
             mt={{ base: 4, md: 0 }}
           >
             <Box m={4} cursor="pointer" onClick={() => handleItemClick("seccion1")}>
-            Sobre nosotros
+              Sobre nosotros
             </Box>
             <Box m={4} cursor="pointer" onClick={() => handleItemClick("seccion2")}>
-            Nuestros Espacios
+              Nuestros Espacios
             </Box>
             <Box m={4} cursor="pointer" onClick={() => handleItemClick("seccion3")}>
               Sección 3
